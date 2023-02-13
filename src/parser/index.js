0b083ca4da2f8202d038b24cc9814431cb25cc67
zackm <zack.mutchler@gmail.com>
2023-02-13 10:27:44 -0600

    fix: updated deprecated setIgnoreTransaction

--- src/parser/index.js
+++ src/parser/index.js
@@ -69,7 +69,7 @@ app.use(function(req, res, next) {
 });
 
 app.get('/healthz', function (req, res) {
-  newrelic.setIgnoreTransaction(true);
+  newrelic.transactionHandle.ignore();
   res.status(200).send('OK');
 });
 
